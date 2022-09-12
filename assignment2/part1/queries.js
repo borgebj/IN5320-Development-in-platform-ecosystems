dfc247b634e38e37107c56704ae00480038a1cac
Børge Bjørnstadjordet <57920815+borgebj@users.noreply.github.com>
2022-09-12 15:54:34 +0200

    part 1

--- assignment2/part1/queries.js
+++ assignment2/part1/queries.js
@@ -1,29 +1,33 @@
 
-window.onload = function() {
-
-    // Elements
-    const submit = document.getElementById("submit")
-    const search = document.getElementById("search")
+// Elements
+const submit = document.getElementById("submit")
+const search = document.getElementById("search")
 
 
-    // Action if submit is pressed
-    submit.addEventListener("click", () => {
-        if (search.value.trim()) {
-            querySearch(search.value)
-        }
-    })
-
-    function querySearch(country) {
-        let query = `https://dhis2-app-course.ifi.uio.no/api?Country=${country}`
-        console.log(`Query statement:\n${query}`)
-        fetch(query, {
-            method: "GET",
-            headers: {
-                    "Content-Type": "application/json"
-                }
-            }
-        )
-            .then(response => response.json())
-            .then(console.log)
+// Action if submit is pressed
+submit.addEventListener("click", () => {
+    if (search.value.trim()) {
+        querySearch(search.value)
     }
+})
+
+function querySearch(country) {
+    let query = `https://dhis2-app-course.ifi.uio.no/api?Country=${country}`
+    query = "https://d6wn6bmjj722w.population.io/1.0/population/Norway/today-and-tomorrow/"
+    fetch(query, {
+        method: "GET",
+        headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    )
+        .then(response => response.json())
+        .then(console.log)
 }
+
+/*
+let query = `https://dhis2-app-course.ifi.uio.no/api?Country=${country}`
+query = "https://d6wn6bmjj722w.population.io/1.0/population/Norway/today-and-tomorrow/"
+const f = fetch(query).then((response) => response.json())
+console.log(await f)
+ */

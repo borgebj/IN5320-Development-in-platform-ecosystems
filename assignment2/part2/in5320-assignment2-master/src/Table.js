2b134b3f030b50d7ec6dbb49d2026fcca4e3a291
Børge Bjørnstadjordet <borgetull@gmail.com>
2022-09-16 03:00:09 +0200

    Step 1 complete

--- assignment2/part2/in5320-assignment2-master/src/Table.js
+++ assignment2/part2/in5320-assignment2-master/src/Table.js
@@ -5,9 +5,26 @@ function Table(props) {
     // If the API request isn't completed return "loading...""
     return <p>Loading...</p>;
   } else {
-    // Write your code here:
+    // Step 1: Creating a table-component for API-data to show
     return <table>
-
+      <thead>
+        <tr>
+          <th>Country</th>
+          <th>Continent</th>
+          <th>Population</th>
+          <th>Population Growth</th>
+        </tr>
+      </thead>
+      <tbody>
+      {props.apiData.results.map((data) => (
+          <tr>
+            <td>{data.Country}</td>
+            <td>{data.Continent}</td>
+            <td>{data.Population}</td>
+            <td>{data.PopulationGrowth}</td>
+          </tr>
+      ))}
+      </tbody>
     </table>;
   }
 }
